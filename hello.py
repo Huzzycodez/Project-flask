00c009efaeecaab7d586389c735b38cb69a2e3ec
Huzzy codez <husseinyakub188@gmail.com>
2023-12-22 13:00:44 +0100

    Commit changes

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "Huzzycodez"
+class NameForm(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("submit")
 
 @app.route('/')
 
@@ -33,3 +41,15 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
    return render_template("500.html"), 404
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+      name = None
+      form = NameForm()
+      if form.validate_on_submit():
+          name = form.name.Data
+          form.name.data = ''
+
+      return render_template("form.html", 
+       name = name,
+       form = form)
